ea9026a003c9533d86df238ffaf13caba1b6b24d
Andrew <36772492+klimkovskyi@users.noreply.github.com>
2024-03-13 21:10:14 +0200

    hw 2 js done

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,11 @@
-function getElementWidth(content, padding, border) {
-    const result = Number.parseFloat(content) + Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2;
-    return (result);
+function checkForSpam(message) {
+    return (message.toLowerCase().includes('spam') || message.toLowerCase().includes('sale')); 
 }
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+
+console.log(checkForSpam("Latest technology news"));
+console.log(checkForSpam("JavaScript weekly newsletter"));
+console.log(checkForSpam("Get best sale offers now!"));
+console.log(checkForSpam("Amazing SalE, only tonight!"));
+console.log(checkForSpam("Trust me, this is not a spam message"));
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!"));
+console.log(checkForSpam("[SPAM] How to earn fast money?"));
